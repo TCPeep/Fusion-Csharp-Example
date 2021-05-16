d3f4296d7e90aa3d4fb6b45df313eeb3f7a349d2
TCPeep <66202922+TCPeep@users.noreply.github.com>
2021-05-16 16:44:55 -0400

    Add files via upload

---
+++ FusionAPI-WPF-CSharp/ChatBubbles/ucReceivedBubble.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FusionAPI_WPF_CSharp.ChatBubbles
+{
+    /// <summary>
+    /// Interaction logic for ucSentBubble.xaml
+    /// </summary>
+    public partial class ucReceivedBubble : UserControl
+    {
+        public Fusion.ChatResponse message;
+
+        public ucReceivedBubble(Fusion.ChatResponse message)
+        {
+            InitializeComponent();
+            lblTitle.Content = message.Author[1];
+            tbMessage.Text = message.Content;
+        }
+    }
+}
